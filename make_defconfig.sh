7f37d3ce9e5e784027708b3097e80360cbdd4e05
mifl <mifl.pc@gmail.com>
2016-10-06 13:45:35 +0200

    make_defconfig: add support for arm64.
    ./make_defconfig.sh {subarch} {product}_defconfig
    
    Change-Id: Icb488c8f7af6de92a1068d8c4cc283bff5d1cfdb
    Signed-off-by: mifl <mifl.pc@gmail.com>

--- make_defconfig.sh
+++ make_defconfig.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-DEFCONFIG_FILE=$1
+BUILD_SUBARCH=$1
+if [ "$BUILD_SUBARCH" = "arm" ]; then
+	GCC_SUBDIR=arm-eabi
+elif [ "$BUILD_SUBARCH" = "arm64" ]; then
+	GCC_SUBDIR=aarch64-linux-android
+fi
+
+DEFCONFIG_FILE=$2
 if [ -z "$DEFCONFIG_FILE" ]; then
 	echo "Need defconfig file(msm_defconfig)!"
 	exit -1
 fi
 
-if [ ! -e arch/arm/configs/$DEFCONFIG_FILE ]; then
-	echo "No such file : arch/arm/configs/$DEFCONFIG_FILE"
+if [ ! -e arch/$BUILD_SUBARCH/configs/$DEFCONFIG_FILE ]; then
+	echo "No such file : arch/$BUILD_SUBARCH/configs/$DEFCONFIG_FILE"
 	exit -1
 fi
 
 # make .config
 env KCONFIG_NOTIMESTAMP=true \
-make ARCH=arm CROSS_COMPILE=arm-eabi- ${DEFCONFIG_FILE}
+make ARCH=$BUILD_SUBARCH CROSS_COMPILE=$GCC_SUBDIR- ${DEFCONFIG_FILE}
 
 # run menuconfig
 env KCONFIG_NOTIMESTAMP=true \
-make menuconfig ARCH=arm
-make savedefconfig ARCH=arm
+make menuconfig ARCH=$BUILD_SUBARCH
+make savedefconfig ARCH=$BUILD_SUBARCH
 
 # copy .config to defconfig
-mv defconfig arch/arm/configs/${DEFCONFIG_FILE}
+mv defconfig arch/$BUILD_SUBARCH/configs/${DEFCONFIG_FILE}
 
 # clean kernel object
 make mrproper
